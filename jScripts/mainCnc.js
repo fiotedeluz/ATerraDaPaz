72611e0729e3636554e92859505467963892a762
Gleidson <v3vgleid@gmail.com>
2023-07-19 23:19:17 -0300

    voltando ao js

---
+++ jScripts/mainCnc.js
@@ -0,0 +1,6 @@
+console.log ("transcrevendo pensamentos")
+// alteração teste
+console.log (" iniciando as abas secundarias")
+// registro de progresso
+console.log ("criando footer header e multiabas")
+// registro de progresso
